2f01f46db2c1251b0cb8fd403c06bb894993770c
Dewanto Krisna <dewanto.krisna@gmail.com>
2020-03-30 20:48:24 +0700

    Membuat Input/Insert Data ke Database Menggunakan Modal Bootstrap

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -24,4 +24,20 @@ class Mahasiswa_model
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa
+                    VALUES 
+                    ('', :nama, :nrp, :email, :jabatan)";
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jabatan', $data['jabatan']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,13 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+                Tambah Data Mahasiswa
+            </button>
+            <br><br>
+
             <h3>Daftar Mahasiswa </h3>
+
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mhs) : ?>
                     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -13,6 +19,52 @@
 
 
 
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nrp">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="jabatan">Jabatan</label>
+                        <select class="form-control" id="jabatan" name="jabatan">
+                            <option value="CEO">CEO</option>
+                            <option value="CTO">CTO</option>
+                            <option value="CFO">CFO</option>
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
