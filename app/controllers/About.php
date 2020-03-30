451fa30149c2514e29d315e7683c4aff86bdebf4
Dewanto Krisna <dewantokrisna@gmail.com>
2020-03-30 11:34:57 +0700

    Menambah file pada folder controllers dan method di folder app

---
+++ app/controllers/About.php
@@ -0,0 +1,18 @@
+<?php
+
+class About
+{
+    // public function index($nama = 'Dietrich', $pekerjaan = 'CEO')
+    // {
+    //     echo "Halo saya $nama, Saya seorang $pekerjaan";
+    // }
+
+    public function page()
+    {
+        echo 'About/page';
+    }
+    public function door()
+    {
+        echo 'About/door';
+    }
+}
---
+++ app/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+class Home
+{
+    public function index()
+    {
+        echo 'Home/index';
+    }
+    public function page()
+    {
+        echo 'Home/page';
+    }
+}
