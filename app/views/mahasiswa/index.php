e46611ecdedaa2b870ec1b962d1b1aec31e611a0
Dewanto Krisna <dewantokrisna@gmail.com>
2020-03-31 09:42:44 +0700

    Menambahkan fungsi search

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -6,12 +6,29 @@
         </div>
     </div>
 
-    <div class="row">
+    <div class="row mb-3">
         <div class="col-lg-6">
             <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
                 Tambah Data Mahasiswa
             </button>
-            <br><br>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-lg-6">
+            <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cari mahasiswa.." name="keyword" id="keyword" autocomplete="off">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
 
             <h3>Daftar Mahasiswa </h3>
 
